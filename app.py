60f3cd3c3df21bfa4918d66d81bb83bcb4f4689c
alonsolopezjeronimo66-cpu <alonsolopezjeronimo66@gmail.com>
2025-10-16 13:43:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
         
 
 
-broker="157.230.214.127"
+broker="broker.mqttdashboard.com"
 port=1883
 client1= paho.Client("GIT-HUB")
 client1.on_message = on_message
@@ -48,11 +48,11 @@ else:
 
 if st.button('OFF'):
     act1="OFF"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("GIT-HUB_jero")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("cmqtt_sjero", message)
   
     
 else:
@@ -62,11 +62,11 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("GIT-HUB_jero")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("cmqtt_a", message)
+    ret= client1.publish("cmqtt_ajero", message)
     
  
 else:
